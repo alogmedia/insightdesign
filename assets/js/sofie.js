fa3e2c5f0d54ea57b1674a447379250fec25787d
alogmedia <albertlogn@gmail.com>
2023-12-11 15:28:30 +0100

    opdateringer

--- assets/js/sofie.js
+++ assets/js/sofie.js
@@ -7,12 +7,12 @@ document.addEventListener("DOMContentLoaded", function() {
     openButton.forEach(function(button) {
       button.addEventListener("click", function() {
         console.log(String(button));
-        if (button.Includes("phknap")){
-            console.log("Sesam");
-    } else {
-        console.log("lort");
-        return
-    }
+          if (button.id.includes("phknap")) {
+              overlay.style.display = "block"; // Assuming you want to show the overlay
+              console.log("Sesam");
+          } else {
+              console.log("lort");
+          }
       });
     });
   
